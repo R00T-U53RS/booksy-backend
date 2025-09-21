7e953f45c2bf28b5e47cb63cd54094ce3e85a7a5
Z3RO-O <vaibhav.singh@fyle.in>
2025-09-21 14:13:50 +0530

    feat: add auth and pre-commit hook

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
+const DEFAULT_PORT = 3000;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { abortOnError: false });
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? DEFAULT_PORT);
 }
-bootstrap();
+
+void bootstrap();
